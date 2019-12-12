8601ab835395ddc8a309c4e7c62600dcb28d886e
forrestlin <forrest_lin@htc.com>
2019-12-12 15:36:29 +0800

    istio

--- istio-install.sh
+++ istio-install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 
+export ISTIO_VERSION='1.4.2'
+
 cd ./0.0--setup-istio
 
 kubectl apply -f ./ns.yaml
 
 printf '\n== Applying: "%s" to kube-cluster ...\n' "istio-crds"
-kubectl apply -f ./istio-crds/
+helm template $HOME/istio-$ISTIO_VERSION/install/kubernetes/helm/istio-init --name-template istio-init --namespace istio-system | kubectl apply -f -
 
 printf '\n== Waiting for all istio CRDs to be created ...\n'
 kubectl -n istio-system wait --for=condition=complete job --all
 
 printf '\n== Applying: "%s" to kube-cluster ...\n' "istio-components"
-kubectl apply -f ./istio-install.yml
+helm template $HOME/istio-$ISTIO_VERSION/install/kubernetes/helm/istio --name-template istio --namespace istio-system | kubectl apply -f -
 
 printf '\n== Applying: "%s" to kube-cluster ...\n' "istio-ingressgateway"
 kubectl apply -f ./istio-ingressgateway/
